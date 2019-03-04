8da4f3ccc269f0ab8b1d13a57c7538a8213a29d6
brenthompson2 <brenthompson2@gmail.com>
2019-03-04 02:34:42 -0500

    Clean & Refactor Script =================
    - Updated comments
    - Fixed indentation
    - Re-organized date parsing

--- SortPhotosByMonth.ps1
+++ SortPhotosByMonth.ps1
@@ -1,19 +1,14 @@
-﻿# ==============================================================================================
+﻿# ============================================================================================== 
+# NAME: PhotosByMonth.ps1
 # 
-# Microsoft PowerShell Source File -- Created with SAPIEN Technologies PrimalScript 4.1
-# 
-# NAME: OrgPhotos.ps1
-# 
-# UPDATED: Steve Smith
-# DATE: 18 January 2009
-# COMMENT: Changed file paths and confirmed it works.  Note that file extension must be .psONE not .psELL
+# UPDATED: Brendan Thompson
+# DATE: 03 March 2019
 #
 # AUTHOR:  Kim Oppalfens, 
 # DATE  : 12/2/2007
 # 
-# COMMENT: Helps you organise your digital photos into subdirectory, based on the Exif data 
-# found inside the picture. Based on the date picture taken property the pictures will be organized into
-# c:\RecentlyUploadedPhotos\YYYY\YYYY-MM-DD
+# COMMENT: Helps you organise your digital photos into subdirectories based on the picture's Exif meta data 
+# Based on the date picture taken property the pictures will be organized into c:\RecentlyUploadedPhotos\YYYY\YYYY-MM
 # ============================================================================================== 
 
 [reflection.assembly]::loadfile( "C:\Windows\Microsoft.NET\Framework\v2.0.50727\System.Drawing.dll") 
@@ -21,22 +16,24 @@
 $Files = Get-ChildItem -recurse -filter *.jpg
 foreach ($file in $Files) 
 {
-  $foo=New-Object -TypeName system.drawing.bitmap -ArgumentList $file.fullname 
+  # Get the image as a bitmap
+  $imageAsBitmap = New-Object -TypeName system.drawing.bitmap -ArgumentList $file.fullname 
 
-#each character represents an ascii code number 0-10 is date 
-#10th character is space separator between date and time
-#48 = 0 49 = 1 50 = 2 51 = 3 52 = 4 53 = 5 54 = 6 55 = 7 56 = 8 57 = 9 58 = : 
-#date is in YYYY/MM/DD format
-  $date = $foo.GetPropertyItem(36867).value[0..9]
-  $arYear = [Char]$date[0],[Char]$date[1],[Char]$date[2],[Char]$date[3]
-  $arMonth = [Char]$date[5],[Char]$date[6]
-  $arDay = [Char]$date[8],[Char]$date[9]
-  $strYear = [String]::Join("",$arYear)
-  $strMonth = [String]::Join("",$arMonth) 
-  $strDay = [String]::Join("",$arDay)
-  $DateTaken = $strYear + "-" + $strMonth + "-" + $strDay
-  $TargetPath = "c:\RecentlyUploadedPhotos\" + $strYear + "\" + $DateTaken
-If (Test-Path $TargetPath)
+  # Parse the date from the bitmap
+  $date = $imageAsBitmap.GetPropertyItem(36867).value[0..9]  
+  $yearArray = [Char]$date[0],[Char]$date[1],[Char]$date[2],[Char]$date[3]
+  $year = [String]::Join("",$yearArray)
+  $monthArray = [Char]$date[5],[Char]$date[6]
+  $month = [String]::Join("",$monthArray)
+  $dayArray = [Char]$date[8],[Char]$date[9]
+  $day = [String]::Join("",$dayArray)
+
+  # Get the folder name from the date
+  $DateTaken = $year + "-" + $month + "-" + $day
+  $TargetPath = "c:\RecentlyUploadedPhotos\" + $year + "\" + $DateTaken
+  
+  # Copy the file to new folder
+  If (Test-Path $TargetPath)
   {
     xcopy /Y/Q $file.FullName $TargetPath
   }
